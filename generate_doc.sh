00a888cb63d79e3ca365e6aee30970008a6cafae
martin-e91 <martin.essuman@gmail.com>
2021-08-15 00:12:42 +0200

    Fix

--- generate_doc.sh
+++ generate_doc.sh
@@ -2,4 +2,4 @@
 rm -rf docs
 
 # Generate documentation.
-swift-doc generate Sources/MGENetwork --output docs --module-name MGENetwork --format html --base-url ./
+swift-doc generate Sources/MGENetwork --output Documentation --module-name MGENetwork --format html --base-url "./Documentation"
