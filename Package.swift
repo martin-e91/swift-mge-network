1b7144defd32113b5d09950ba45c7ec45f0ae398
martin-e91 <martin.essuman@gmail.com>
2021-08-14 22:17:49 +0200

    Updated MGELogger

--- Package.swift
+++ Package.swift
@@ -10,21 +10,13 @@ let package = Package(
   ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    .library(
-      name: "MGENetwork",
-      targets: ["MGENetwork"]),
+    .library(name: "MGENetwork", targets: ["MGENetwork"]),
   ],
   dependencies: [
-    .package(url: "git@github.com:martin-e91/MGELogger.git", from: "0.1.0")
+    .package(url: "git@github.com:martin-e91/MGELogger.git", from: "0.8.0")
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(
-      name: "MGENetwork",
-      dependencies: ["MGELogger"]),
-    .testTarget(
-      name: "MGENetworkTests",
-      dependencies: ["MGENetwork"]),
+    .target(name: "MGENetwork", dependencies: ["MGELogger"]),
+    .testTarget(name: "MGENetworkTests", dependencies: ["MGENetwork", "MGELogger"]),
   ]
 )
