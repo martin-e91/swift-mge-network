6375bde950d0d629223453515b3e4ef133bb924f
martin-e91 <martin.essuman@gmail.com>
2021-08-15 01:12:36 +0200

    clean

--- Package.swift
+++ Package.swift
@@ -18,9 +18,7 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(
-      name: "MGENetwork",
-      dependencies: ["MGELogger"]),
+    .target(name: "MGENetwork", dependencies: ["MGELogger"], exclude: ["docs"]),
     .testTarget(
       name: "MGENetworkTests",
       dependencies: ["MGENetwork"]),
